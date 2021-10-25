20800168e832605315ab42112884b05ce1bc5a9c
MX-Coder <54361746+lalit123-blip@users.noreply.github.comdev>
2021-10-25 10:26:36 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Send an Email</title>
+</head>
+<body>
+
+	
+		
+
+		<form id="myForm">
+			<h2>Send an Email</h2>
+
+			<label>Name</label>
+			<input id="name" type="text" placeholder="Enter Name">
+			<br><br>
+
+			<label>Email</label>
+			<input id="email" type="text" placeholder="Enter Email">
+			<br><br>
+
+			<label>Subject</label>
+			<input id="subject" type="text" placeholder=" Enter Subject"> 
+			<br><br>
+
+			<p>Message</p>
+			<textarea id="body" rows="5" placeholder="Type Message"></textarea>
+			<br><br>
+
+			<button type="button" onclick="sendEmail()" value="Send An Email">Submit</button> 
+		</form>
+        <p class="sent-notification"></p>
+
+	<script src="http://code.jquery.com/jquery-3.4.1.min.js"></script>
+	<script type="text/javascript">
+        function sendEmail() {
+            var name = $("#name");
+            var email = $("#email");
+            var subject = $("#subject");
+            var body = $("#body");
+
+            if (isNotEmpty(name) && isNotEmpty(email) && isNotEmpty(subject) && isNotEmpty(body)) {
+                $.ajax({
+                   url: 'sendEmail.php',
+                   method: 'POST',
+                   dataType: 'json',
+                   data: {
+                       name: name.val(),
+                       email: email.val(),
+                       subject: subject.val(),
+                       body: body.val()
+                   }, success: function (response) {
+                        $('#myForm')[0].reset();
+                        $('.sent-notification').text("Message Sent Successfully.");
+                   }
+                });
+            }
+        }
+
+        function isNotEmpty(caller) {
+            if (caller.val() == "") {
+                caller.css('border', '1px solid red');
+                return false;
+            } else
+                caller.css('border', '');
+
+            return true;
+        }
+    </script>
+
+</body>
+</html>
